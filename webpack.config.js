e3d8b6e978dfc25e132825f48c510d087da28653
FusedFormations <info@fusedformations.com>
2025-05-18 10:41:25 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,8 @@ const path = require('path')
 module.exports = {
   entry: './dist/test.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'test'),
-  },
-  resolve: { alias: { stream: 'stream-browserify', buffer: 'buffer', process: 'process/browser' } },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: 'process',
-    }),
-  ],
+  filename: 'main.js',
+  path: path.resolve(__dirname, 'dist'),
+  library: 'ArduinoWeb', 
+  libraryTarget: 'window', 
 }
