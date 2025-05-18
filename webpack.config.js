83e6e801fcc48f47855e6f5d980e2fb653057a60
FusedFormations <info@fusedformations.com>
2025-05-18 10:48:29 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
-const webpack = require('webpack')
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  entry: './dist/test.js',
+  mode: 'production',
+  entry: './dist/index.js', // after tsc builds index.ts
   output: {
-  filename: 'main.js',
-  path: path.resolve(__dirname, 'dist'),
-  library: 'ArduinoWeb', 
-  libraryTarget: 'window', 
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'ArduinoWeb',
+    libraryTarget: 'window' // makes ArduinoWeb available globally
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
-}
+};
+
